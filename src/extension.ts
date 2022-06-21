f47f1302d865afe6cc250e72267a99b8198347bb
GenLike <ricardo.f.oliveira.da.silva@tecnico.ulisboa.pt>
2022-06-21 15:33:41 +0100

    transportkind

--- src/extension.ts
+++ src/extension.ts
@@ -19,7 +19,7 @@ export function activate(context: ExtensionContext) {
     console.log(script);
     fs.chmod(script, "001", () => {
         let serverOptions: ServerOptions = {
-            run : { command: script, transport: TransportKind.ipc },
+            run : { command: script, transport: TransportKind.socket },
             debug: { command: script, args: [], options: { env: createDebugEnv() } }
         };
         
