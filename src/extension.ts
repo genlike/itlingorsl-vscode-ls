ca98898ded4073de368bc73a3044f349ed4b6661
GenLike <ricardo.f.oliveira.da.silva@tecnico.ulisboa.pt>
2022-06-21 13:28:12 +0100

    try fix

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@
 
 import * as path from 'path';
 import * as os from 'os';
+import * as fs from 'fs';
 
 import {Trace} from 'vscode-jsonrpc';
 import { commands, window, workspace, ExtensionContext, Uri } from 'vscode';
@@ -16,48 +17,43 @@ export function activate(context: ExtensionContext) {
     let script = context.asAbsolutePath(path.join('src', 'mydsl', 'bin', launcher));
     console.log("SCRIPT");
     console.log(script);
-    const cp = require('child_process')
-    cp.exec('chmod +x ' + script, (err, stdout, stderr) => {
-        console.log('stdout: ' + stdout);
-        console.log('stderr: ' + stderr);
-        if (err) {
-            console.log('error: ' + err);
-        }
-    });
-    let serverOptions: ServerOptions = {
-        run : { command: script },
-        debug: { command: script, args: [], options: { env: createDebugEnv() } }
-    };
+    fs.chmod(script, "+x", () => {
+        let serverOptions: ServerOptions = {
+            run : { command: script },
+            debug: { command: script, args: [], options: { env: createDebugEnv() } }
+        };
+        
+        let clientOptions: LanguageClientOptions = {
+            documentSelector: ['itlang'],
+            synchronize: {
+                fileEvents: workspace.createFileSystemWatcher('**/*.*')
+            }
+        };
+        
+        // Create the language client and start the client.
+        let lc = new LanguageClient('Xtext Server', serverOptions, clientOptions);
+        
+        var disposable2 =commands.registerCommand("itlang.a.proxy", async () => {
+            let activeEditor = window.activeTextEditor;
+            if (!activeEditor || !activeEditor.document || activeEditor.document.languageId !== 'itlang') {
+                return;
+            }
     
-    let clientOptions: LanguageClientOptions = {
-        documentSelector: ['itlang'],
-        synchronize: {
-            fileEvents: workspace.createFileSystemWatcher('**/*.*')
-        }
-    };
-    
-    // Create the language client and start the client.
-    let lc = new LanguageClient('Xtext Server', serverOptions, clientOptions);
-    
-    var disposable2 =commands.registerCommand("itlang.a.proxy", async () => {
-        let activeEditor = window.activeTextEditor;
-        if (!activeEditor || !activeEditor.document || activeEditor.document.languageId !== 'itlang') {
-            return;
-        }
-
-        if (activeEditor.document.uri instanceof Uri) {
-            commands.executeCommand("itlang.a", activeEditor.document.uri.toString());
-        }
+            if (activeEditor.document.uri instanceof Uri) {
+                commands.executeCommand("itlang.a", activeEditor.document.uri.toString());
+            }
+        })
+        context.subscriptions.push(disposable2);
+        
+        // enable tracing (.Off, .Messages, Verbose)
+        lc.trace = Trace.Verbose;
+        let disposable = lc.start();
+        
+        // Push the disposable to the context's subscriptions so that the 
+        // client can be deactivated on extension deactivation
+        context.subscriptions.push(disposable);
     })
-    context.subscriptions.push(disposable2);
-    
-    // enable tracing (.Off, .Messages, Verbose)
-    lc.trace = Trace.Verbose;
-    let disposable = lc.start();
-    
-    // Push the disposable to the context's subscriptions so that the 
-    // client can be deactivated on extension deactivation
-    context.subscriptions.push(disposable);
+
 }
 
 function createDebugEnv() {
