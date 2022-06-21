fae9d24dd5a1df396d2c6926b49e092a53e5d99b
GenLike <ricardo.f.oliveira.da.silva@tecnico.ulisboa.pt>
2022-06-21 14:58:55 +0100

    rever changes

--- src/extension.ts
+++ src/extension.ts
@@ -1,53 +1,63 @@
 'use strict';
 
-import * as net from 'net';
+import * as path from 'path';
+import * as os from 'os';
+import * as fs from 'fs';
 
 import {Trace} from 'vscode-jsonrpc';
-import { window, workspace, commands, ExtensionContext, Uri } from 'vscode';
-import { LanguageClient, LanguageClientOptions, StreamInfo, Position as LSPosition, Location as LSLocation } from 'vscode-languageclient/node';
+import { commands, window, workspace, ExtensionContext, Uri } from 'vscode';
+import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient/node';
+
+
 
 export function activate(context: ExtensionContext) {
-    // The server is a started as a separate app and listens on port 5007
-    let connectionInfo = {
-        port: 5007
-    };
-    let serverOptions = () => {
-        // Connect to language server via socket
-        let socket = net.connect(connectionInfo);
-        let result: StreamInfo = {
-            writer: socket,
-            reader: socket
+    // The server is a locally installed in src/mydsl
+    
+    let launcher = os.platform() === 'win32' ? 'start-ls-itlingo.bat' : 'start-ls-itlingo';
+    let script = context.asAbsolutePath(path.join('src', 'mydsl', 'bin', launcher));
+    console.log("SCRIPT");
+    console.log(script);
+    fs.chmod(script, "001", () => {
+        let serverOptions: ServerOptions = {
+            run : { command: script },
+            debug: { command: script, args: [], options: { env: createDebugEnv() } }
         };
-        return Promise.resolve(result);
-    };
-    
-    let clientOptions: LanguageClientOptions = {
-        documentSelector: ['itlang'],
-        synchronize: {
-            fileEvents: workspace.createFileSystemWatcher('**/*.*')
-        }
-    };
-    
-   // Create the language client and start the client.
-   let lc = new LanguageClient('Xtext Server', serverOptions, clientOptions);
         
-   var disposable2 =commands.registerCommand("itlang.a.proxy", async () => {
-       let activeEditor = window.activeTextEditor;
-       if (!activeEditor || !activeEditor.document || activeEditor.document.languageId !== 'itlang') {
-           return;
-       }
+        let clientOptions: LanguageClientOptions = {
+            documentSelector: ['itlang'],
+            synchronize: {
+                fileEvents: workspace.createFileSystemWatcher('**/*.*')
+            }
+        };
+        
+        // Create the language client and start the client.
+        let lc = new LanguageClient('Xtext Server', serverOptions, clientOptions);
+        
+        var disposable2 =commands.registerCommand("itlang.a.proxy", async () => {
+            let activeEditor = window.activeTextEditor;
+            if (!activeEditor || !activeEditor.document || activeEditor.document.languageId !== 'itlang') {
+                return;
+            }
+    
+            if (activeEditor.document.uri instanceof Uri) {
+                commands.executeCommand("itlang.a", activeEditor.document.uri.toString());
+            }
+        })
+        context.subscriptions.push(disposable2);
+        
+        // enable tracing (.Off, .Messages, Verbose)
+        lc.trace = Trace.Verbose;
+        let disposable = lc.start();
+        
+        // Push the disposable to the context's subscriptions so that the 
+        // client can be deactivated on extension deactivation
+        context.subscriptions.push(disposable);
+    });
 
-       if (activeEditor.document.uri instanceof Uri) {
-           commands.executeCommand("itlang.a", activeEditor.document.uri.toString());
-       }
-   })
-   context.subscriptions.push(disposable2);
-   
-   // enable tracing (.Off, .Messages, Verbose)
-   lc.trace = Trace.Verbose;
-   let disposable = lc.start();
-   
-   // Push the disposable to the context's subscriptions so that the 
-   // client can be deactivated on extension deactivation
-   context.subscriptions.push(disposable);
+}
+
+function createDebugEnv() {
+    return Object.assign({
+        JAVA_OPTS:"-Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=8000,suspend=n,quiet=y"
+    }, process.env)
 }
