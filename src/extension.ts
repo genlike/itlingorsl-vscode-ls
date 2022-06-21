28e698339733645f52b77992d1f9a3e3a0039deb
GenLike <ricardo.f.oliveira.da.silva@tecnico.ulisboa.pt>
2022-06-21 14:04:09 +0100

    changed to connection

--- src/extension.ts
+++ src/extension.ts
@@ -1,63 +1,53 @@
 'use strict';
 
-import * as path from 'path';
-import * as os from 'os';
-import * as fs from 'fs';
+import * as net from 'net';
 
 import {Trace} from 'vscode-jsonrpc';
-import { commands, window, workspace, ExtensionContext, Uri } from 'vscode';
-import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient/node';
-
-
+import { window, workspace, commands, ExtensionContext, Uri } from 'vscode';
+import { LanguageClient, LanguageClientOptions, StreamInfo, Position as LSPosition, Location as LSLocation } from 'vscode-languageclient/node';
 
 export function activate(context: ExtensionContext) {
-    // The server is a locally installed in src/mydsl
-    
-    let launcher = os.platform() === 'win32' ? 'start-ls-itlingo.bat' : 'start-ls-itlingo';
-    let script = context.asAbsolutePath(path.join('src', 'mydsl', 'bin', launcher));
-    console.log("SCRIPT");
-    console.log(script);
-    fs.chmod(script, "001", () => {
-        let serverOptions: ServerOptions = {
-            run : { command: script },
-            debug: { command: script, args: [], options: { env: createDebugEnv() } }
+    // The server is a started as a separate app and listens on port 5007
+    let connectionInfo = {
+        port: 5007
+    };
+    let serverOptions = () => {
+        // Connect to language server via socket
+        let socket = net.connect(connectionInfo);
+        let result: StreamInfo = {
+            writer: socket,
+            reader: socket
         };
-        
-        let clientOptions: LanguageClientOptions = {
-            documentSelector: ['itlang'],
-            synchronize: {
-                fileEvents: workspace.createFileSystemWatcher('**/*.*')
-            }
-        };
-        
-        // Create the language client and start the client.
-        let lc = new LanguageClient('Xtext Server', serverOptions, clientOptions);
-        
-        var disposable2 =commands.registerCommand("itlang.a.proxy", async () => {
-            let activeEditor = window.activeTextEditor;
-            if (!activeEditor || !activeEditor.document || activeEditor.document.languageId !== 'itlang') {
-                return;
-            }
+        return Promise.resolve(result);
+    };
     
-            if (activeEditor.document.uri instanceof Uri) {
-                commands.executeCommand("itlang.a", activeEditor.document.uri.toString());
-            }
-        })
-        context.subscriptions.push(disposable2);
+    let clientOptions: LanguageClientOptions = {
+        documentSelector: ['itlang'],
+        synchronize: {
+            fileEvents: workspace.createFileSystemWatcher('**/*.*')
+        }
+    };
+    
+   // Create the language client and start the client.
+   let lc = new LanguageClient('Xtext Server', serverOptions, clientOptions);
         
-        // enable tracing (.Off, .Messages, Verbose)
-        lc.trace = Trace.Verbose;
-        let disposable = lc.start();
-        
-        // Push the disposable to the context's subscriptions so that the 
-        // client can be deactivated on extension deactivation
-        context.subscriptions.push(disposable);
-    });
+   var disposable2 =commands.registerCommand("itlang.a.proxy", async () => {
+       let activeEditor = window.activeTextEditor;
+       if (!activeEditor || !activeEditor.document || activeEditor.document.languageId !== 'itlang') {
+           return;
+       }
 
-}
-
-function createDebugEnv() {
-    return Object.assign({
-        JAVA_OPTS:"-Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=8000,suspend=n,quiet=y"
-    }, process.env)
+       if (activeEditor.document.uri instanceof Uri) {
+           commands.executeCommand("itlang.a", activeEditor.document.uri.toString());
+       }
+   })
+   context.subscriptions.push(disposable2);
+   
+   // enable tracing (.Off, .Messages, Verbose)
+   lc.trace = Trace.Verbose;
+   let disposable = lc.start();
+   
+   // Push the disposable to the context's subscriptions so that the 
+   // client can be deactivated on extension deactivation
+   context.subscriptions.push(disposable);
 }
